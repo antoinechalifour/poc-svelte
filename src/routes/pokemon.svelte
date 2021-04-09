8addf9fea828fd10c57069ec12836a2d8ac17de0
Antoine Chalifour <antoine.chalifour@octo.com>
2021-04-09 20:41:34 +0200

    feat: implicit return

--- src/routes/pokemon.svelte
+++ src/routes/pokemon.svelte
@@ -1,10 +1,8 @@
 <script lang='ts' context='module'>
 	import type { Load } from '@sveltejs/kit';
 
-	export const load: Load = () => {
-		return {
-			status: 303,
-			redirect: `/pokemon/${Math.floor(Math.random() * 932) + 1}`
-		};
-	};
+	export const load: Load = () => ({
+		status: 303,
+		redirect: `/pokemon/${Math.floor(Math.random() * 898) + 1}`
+	});
 </script>
